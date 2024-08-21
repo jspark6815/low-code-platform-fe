04a612ca10878d5d4531a5fa409873ad010066b6
jspark6815 <jspark6815@daum.net>
2024-08-21 16:51:09 +0900

    feat: _app page

---
+++ lowcode-connect-platform/src/pages/_app.tsx
@@ -0,0 +1,17 @@
+import theme from '@/styles/theme'
+import { GlobalStyle } from '@/styles/globals'
+import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'styled-components'
+import StyledComponentsRegistry from '@/lib/registry'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <StyledComponentsRegistry>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </StyledComponentsRegistry>
+)}
+
+export default MyApp
